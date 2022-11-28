4ef323ff448ea2f554c0caaaf2bca1fc0a38a6af
Juliana Torres <torresaaronjuliana@gmail.com>
2022-11-27 21:54:48 -0500

    Finalizando ventana Cliente

---
+++ Presentacion_GUI/VisPrincipal.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace TallerMecanico
+{
+    partial class VisPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oPCIONESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.carroToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.motoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sALIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consulToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.auToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.motoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bicicletaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.LimeGreen;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oPCIONESToolStripMenuItem,
+            this.lToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(543, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oPCIONESToolStripMenuItem
+            // 
+            this.oPCIONESToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.regisToolStripMenuItem,
+            this.sALIRToolStripMenuItem});
+            this.oPCIONESToolStripMenuItem.Name = "oPCIONESToolStripMenuItem";
+            this.oPCIONESToolStripMenuItem.Size = new System.Drawing.Size(119, 20);
+            this.oPCIONESToolStripMenuItem.Text = "Gestion de Servicio";
+            // 
+            // regisToolStripMenuItem
+            // 
+            this.regisToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.carroToolStripMenuItem1,
+            this.motoToolStripMenuItem1});
+            this.regisToolStripMenuItem.Name = "regisToolStripMenuItem";
+            this.regisToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.regisToolStripMenuItem.Text = "Registrar Cliente";
+            // 
+            // carroToolStripMenuItem1
+            // 
+            this.carroToolStripMenuItem1.Name = "carroToolStripMenuItem1";
+            this.carroToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.carroToolStripMenuItem1.Text = "Carro";
+            this.carroToolStripMenuItem1.Click += new System.EventHandler(this.carroToolStripMenuItem1_Click);
+            // 
+            // motoToolStripMenuItem1
+            // 
+            this.motoToolStripMenuItem1.Name = "motoToolStripMenuItem1";
+            this.motoToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.motoToolStripMenuItem1.Text = "Moto";
+            // 
+            // sALIRToolStripMenuItem
+            // 
+            this.sALIRToolStripMenuItem.Name = "sALIRToolStripMenuItem";
+            this.sALIRToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.sALIRToolStripMenuItem.Text = "Salir";
+            // 
+            // lToolStripMenuItem
+            // 
+            this.lToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consulToolStripMenuItem,
+            this.salirToolStripMenuItem1});
+            this.lToolStripMenuItem.Name = "lToolStripMenuItem";
+            this.lToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.lToolStripMenuItem.Text = "Listado General";
+            // 
+            // consulToolStripMenuItem
+            // 
+            this.consulToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.auToolStripMenuItem,
+            this.motoToolStripMenuItem,
+            this.bicicletaToolStripMenuItem});
+            this.consulToolStripMenuItem.Name = "consulToolStripMenuItem";
+            this.consulToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.consulToolStripMenuItem.Text = "Consultar Listado";
+            // 
+            // auToolStripMenuItem
+            // 
+            this.auToolStripMenuItem.Name = "auToolStripMenuItem";
+            this.auToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.auToolStripMenuItem.Text = "Auto";
+            // 
+            // motoToolStripMenuItem
+            // 
+            this.motoToolStripMenuItem.Name = "motoToolStripMenuItem";
+            this.motoToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.motoToolStripMenuItem.Text = "Moto";
+            // 
+            // bicicletaToolStripMenuItem
+            // 
+            this.bicicletaToolStripMenuItem.Name = "bicicletaToolStripMenuItem";
+            this.bicicletaToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.bicicletaToolStripMenuItem.Text = "Bicicleta";
+            // 
+            // salirToolStripMenuItem1
+            // 
+            this.salirToolStripMenuItem1.Name = "salirToolStripMenuItem1";
+            this.salirToolStripMenuItem1.Size = new System.Drawing.Size(166, 22);
+            this.salirToolStripMenuItem1.Text = "Salir";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.LimeGreen;
+            this.label2.Location = new System.Drawing.Point(48, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(546, 345);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ". ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LimeGreen;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(175, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BIENVENIDO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LimeGreen;
+            this.label3.Font = new System.Drawing.Font("Bodoni MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(319, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(275, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "JUNIOR RODRIGUEZ-JULIANA TORRES";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Orange;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(-51, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(645, 345);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LimeGreen;
+            this.pictureBox1.Image = global::TallerMecanico.Properties.Resources.pngtree_repair_icon_workshop_mechanic_symbol_png_image_4822021_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(133, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(385, 338);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // VisPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 364);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "VisPrincipal";
+            this.Text = "VisPrincipal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oPCIONESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem regisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem carroToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem motoToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sALIRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consulToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem auToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem motoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bicicletaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
