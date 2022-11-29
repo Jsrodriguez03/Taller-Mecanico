7586412933e1177aff416a2883c8e893193263ba
Junior Rodriguez <juniorsantiagoalvarez03@gmail.com>
2022-11-29 15:29:41 -0500

    Actualizando Capa Datos

--- Datos/RepositorioBicicleta.cs
+++ Datos/RepositorioBicicleta.cs
@@ -7,13 +7,13 @@ namespace Datos
 {
     public class RepositorioBicicleta:BaseDatos
     {
-        public int InsertarVehiculo(Bicicleta bici)
+        public int InsertarBicicleta(Bicicleta bici)
         {
             try
             {
                 int retorno = 0;
-                MySqlCommand comando = new MySqlCommand(string.Format("Insert into  bici(idServicio, Propietario, Marca, Color, Placa) " +
-                "values('{0}','{1}','{2}','{3}','{4}')", bici.idSer.IdServicio, bici.Propietario, bici.Marca, bici.Color, bici.Placa), ObtenerConexion());
+                MySqlCommand comando = new MySqlCommand(string.Format("Insert into  bicicleta(idServicio, Propietario, Marca, Color) " +
+                "values('{0}','{1}','{2}','{3}')", bici.idSer.IdServicio, bici.Propietario, bici.Marca, bici.Color), ObtenerConexion());
 
                 retorno = comando.ExecuteNonQuery();
                 return retorno;
